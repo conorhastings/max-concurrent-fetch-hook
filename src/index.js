2ef4c841e8b30eefde1b2cb98cd7244d16ce2c6d
Conor Hastings <hastings.conorm@gmail.com>
2019-02-26 11:58:08 -0800

    init

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+export default function useMaxConcurrentFetch({
+  url,
+  options,
+  max,
+  handleSuccess,
+  handleError,
+  shouldMakeNewRequest = true
+}) {
+  const [inFlightRequests, setInFlightRequests] = useState([]);
+  const [results, setResult] = useState([]);
+  
+  useEffect(() => {
+    if (shouldMakeNewRequest) {
+      const abortController = new AbortController();
+      setInFlightRequests(abortController);
+      if (inFlightRequests > max) {
+        const requestsToAbortTotal = inFlightRequests - max;
+        for (let i = 0; i < requestsToAbortTotal; i++) {
+          inFlightRequests[i].abort();
+        }
+        setInFlightRequests(inFlightRequests.slice(requestsToAbortTotal));
+      }
+      fetch(url, { signal: abortController.signal, ...options })
+        .then(res => {
+          const result = handleSuccess(res);
+          setResult([...results, result]);
+        })
+        .catch(error => {
+          const result = handleError(error);
+          setReult([...results, result]);
+        });
+    }
+  }, [shouldMakeNewRequest]);
+
+  return results;
+}
